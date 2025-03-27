e8bfa5cb8abb1ab88194725c63daf991490473e5
Bruno Piaggio <brunopiaggiok@gmail.com>
2025-03-27 17:09:50 -0300

    Continue implementing new spend page

--- src/components/pages/spends/sub-pages/new-spend/NewSpend.tsx
+++ src/components/pages/spends/sub-pages/new-spend/NewSpend.tsx
@@ -1,12 +1,123 @@
-import { Typography } from "@mui/material"
-import MainMenu from "../../MainMenu"
-import Page from "../../../../Page"
+import * as Mui from '@mui/material'
+import { useQuery } from '@tanstack/react-query'
+import { useState } from 'react'
+import { getCategories } from '../../../../../api/mis-gastos'
+import Page from '../../../../Page'
+import MainMenu from '../../MainMenu'
+
+const Box = Mui.styled(Mui.Box)<Mui.BoxProps>(() => ({
+  display: 'flex',
+  flex: 0,
+  padding: '1rem'
+}))
+
+const FormControl = Mui.styled(Mui.FormControl)<Mui.FormControlProps>(() => ({
+  flex: 1,
+  width: '100%'
+}))
+
+const TextField = Mui.styled(Mui.TextField)<Mui.TextFieldProps>(() => ({
+  flex: 1
+}))
 
 function NewSpend() {
+  const [age, setAge] = useState('')
+
+  const [group, setGroup] = useState<string>()
+
+  const query = useQuery({ queryKey: ['categories'], queryFn: getCategories, staleTime: Infinity })
+
+  const handleChange = () => {
+    alert('Not implemented')
+  }
+
+  const handleGroupChange = (event: Mui.SelectChangeEvent) => {
+    setGroup(event.target.value)
+  }
+
   return (
-     <Page mainMenu={<MainMenu/>}>
-      <Typography>Work in progress 🚧</Typography>
-     </Page>
+    <Page mainMenu={<MainMenu />}>
+      <FormControl>
+        <Box>
+          <Mui.FormControl variant="standard" fullWidth>
+            <Mui.InputLabel id="category-select-label" >Category</Mui.InputLabel>
+            <Mui.Select
+              labelId="category-select-label"
+              id="demo-simple-select"
+              value={age}
+              label="Category"
+              onChange={handleChange}
+              variant='standard'
+              fullWidth
+            >
+              <Mui.MenuItem value={10}>Ten</Mui.MenuItem>
+              <Mui.MenuItem value={20}>Twenty</Mui.MenuItem>
+              <Mui.MenuItem value={30}>Thirty</Mui.MenuItem>
+            </Mui.Select>
+          </Mui.FormControl>
+        </Box>
+        <Box>
+          <Mui.FormControl variant="standard" fullWidth>
+            <Mui.InputLabel id="demo-simple-select-label">Subcategory</Mui.InputLabel>
+            <Mui.Select
+              labelId="demo-simple-select-label"
+              id="demo-simple-select"
+              value={age}
+              label="Subcategory"
+              onChange={handleChange}
+            >
+              <Mui.MenuItem value={10}>Ten</Mui.MenuItem>
+              <Mui.MenuItem value={20}>Twenty</Mui.MenuItem>
+              <Mui.MenuItem value={30}>Thirty</Mui.MenuItem>
+            </Mui.Select>
+          </Mui.FormControl>
+        </Box>
+        <Box>
+          <Mui.FormControl variant="standard" fullWidth>
+            <Mui.InputLabel id="demo-simple-select-label">Group</Mui.InputLabel>
+            <Mui.Select
+              labelId="demo-simple-select-label"
+              id="demo-simple-select"
+              value={age}
+              label="Group"
+              onChange={handleGroupChange}
+            >
+              {query.data?.map((group) => (
+                <Mui.MenuItem value={group.id} key={group.id}>
+                  {group.name}
+                </Mui.MenuItem>
+              ))}
+            </Mui.Select>
+          </Mui.FormControl>
+        </Box>
+        <Box>
+          <Mui.FormControl variant="standard" fullWidth>
+            <Mui.InputLabel id="demo-simple-select-label">Account</Mui.InputLabel>
+            <Mui.Select
+              labelId="demo-simple-select-label"
+              id="demo-simple-select"
+              value={age}
+              label="Account"
+              onChange={handleChange}
+            >
+              <Mui.MenuItem value={10}>Ten</Mui.MenuItem>
+              <Mui.MenuItem value={20}>Twenty</Mui.MenuItem>
+              <Mui.MenuItem value={30}>Thirty</Mui.MenuItem>
+            </Mui.Select>
+          </Mui.FormControl>
+        </Box>
+        <Box>
+          <TextField
+            id="timeout-textfield"
+            label="Value"
+            defaultValue="0"
+            variant="filled"
+            type="number"
+          />
+        </Box>
+        <Mui.Typography>Work in progress 🚧</Mui.Typography>
+      </FormControl>
+    </Page>
   )
 }
 
