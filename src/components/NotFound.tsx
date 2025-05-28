18d32bd220ed613e1d0df8ef47303f5772c46d24
Bruno Piaggio <brunopiaggiok@gmail.com>
2025-05-28 12:33:20 -0300

    Improve not found page

--- src/components/NotFound.tsx
+++ src/components/NotFound.tsx
@@ -1,5 +1,11 @@
-import Typography from '@mui/material/Typography'
+import { styled, Typography as MuiTypography, TypographyProps as MuiTypographyProps } from '@mui/material'
 import Page from './Page'
+
+const Typography = styled(MuiTypography)<MuiTypographyProps>(() => ({
+  display: 'flex',
+  justifyContent: 'center',
+  padding: '1rem'
+}))
 
 function NotFound() {
   return (
